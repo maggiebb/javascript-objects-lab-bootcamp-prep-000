0ff7a074d7491d4b9fa367a59ede1c5218432cb3
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-03 23:19:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 var recipes = newObject();
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},obj,{[key]:value})
+  var recipe ={eggs:1}
+}
+updateObjectWithKeyAndValue (recipes,recipe)
